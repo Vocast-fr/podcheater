66ead6e7ef64481c534740899ec665c83507ffc1
Anthony <a.gourraud@mediameeting.fr>
2020-08-27 13:51:17 +0200

    replace for loops with promise.all + random global UA

--- index.js
+++ index.js
@@ -1,61 +1,30 @@
 require('dotenv').config()
 
-const { DEV, GOOGLE_CLOUD_PROJECT, MIN_NB_BYTES, WAIT } = process.env
-
-const publicIp = require('public-ip')
-const request = require('superagent')
+const { GOOGLE_CLOUD_PROJECT, WAIT } = process.env
 
 const { rebootModem } = require('./lib/modem')
-const pickEpisodes = require('./lib/pickEpisodes')
-const pickUA = require('./lib/pickUA')
-const { ddlPartOfBinary } = require('./lib/utils')
+const { pickAndRequest } = require('./lib/pickEpisodes')
 const {
   sleep,
   sleepFromOhConfig,
   waitUntilConnectionIsUp
 } = require('./lib/wait')
-
 const insertInBQ = GOOGLE_CLOUD_PROJECT ? require('./lib/insertInBQ') : null
-
-const pickAndRequest = async () => {
-  const IP = await publicIp.v4()
-  const pickedEpisodes = await pickEpisodes()
-  // const pickedUA = await pickUA()
-
-  for (let i = 0; i < pickedEpisodes.length; i++) {
-    const { episodeUrl, episodeTitle } = pickedEpisodes[i]
-    try {
-      // let's try another UA for each request ?
-      const pickedUA = await pickUA()
-
-      await ddlPartOfBinary(
-        request.get(episodeUrl).set('User-Agent', pickedUA),
-        MIN_NB_BYTES
-      )
-      Object.assign(pickedEpisodes[i], {
-        requestDate: new Date(),
-        IP,
-        UA: pickedUA
-      })
-    } catch (e) {
-      console.error(`cannot ddl ${episodeTitle}`, e)
-    }
-  }
-
-  return pickedEpisodes.filter(({ requestDate }) => requestDate)
-}
 
 const unitProcess = async () => {
   try {
     const downloads = await pickAndRequest()
-    if (!DEV && insertInBQ && downloads.length) await insertInBQ(downloads)
-    if (!DEV) await rebootModem()
+
+    // console.log({ downloads })
+
+    if (insertInBQ && downloads.length) await insertInBQ(downloads)
+
+    await rebootModem()
     await waitUntilConnectionIsUp()
-    if (!DEV) {
-      await sleepFromOhConfig(WAIT)
-    } else {
-      await sleep(10)
-    }
+
+    await sleepFromOhConfig(WAIT)
+
+    // await sleep(10)
   } catch (e) {
     console.error('Error in the unit process', e)
   }
